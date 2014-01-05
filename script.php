c810d31fd30ac0da51b6dc621fd4ebf7d113f35f
djbrooks111 <djbrooks111@gmail.com>
2014-01-05 13:48:47 -0500

    Initial Commit

---
+++ script.php
@@ -0,0 +1,69 @@
+<?php
+// Script to automatically get the current price of Cex.io
+// and then tweet that price
+
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 David Brooks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+// Getting the price from Cex.io
+require_once("cexapi.class.php");
+
+$username 	= "username";
+$api_key	= "api_key";
+$api_secret	= "api_secret";
+
+$api = new cexapi($username, $api_key, $api_secret);
+$tick = $api->ticker("GHS/BTC");
+var_dump($tick);
+$price = $tick["last"];
+
+echo "Current Price: $price";
+
+//Tweeting part of the script
+require_once("twitter.class.php");
+
+$consumerKey    = "consumerKey";
+$consumerSecret = "consumerSecret";
+$oAuthToken     = "oAuthToken";
+$oAuthSecret    = "oAuthSecret";
+
+$twitter = new Twitter($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
+
+try {
+	$tweet = $twitter->send("The price of GHS/BTC on Cex.io is now $price");
+} catch (TwitterException $e) {
+	echo "Error: " . $e->getMessage();
+	$compare = "Status is a duplicate.";
+	if (strcmp($e->getMessage(), $compare) == 0) {
+		echo "Resending different status";
+		try {
+			$tweetTwo = $twitter->send("The price of GHS/BTC on Cex.io is still $price");
+		} catch (TwitterException $eTwo) {
+			echo "ErrorTwo: " . $eTwo->getMessage();
+		}
+	}
+}
+
+?>
